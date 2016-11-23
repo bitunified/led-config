1ce3287daa21da6df31f1f3149cd8290779db6bf
BertLaptop <Bert@laptop>
2016-11-23 20:13:32 +0100

    Tomcat

--- server/src/main/java/com/bitunified/server/Application.java
+++ server/src/main/java/com/bitunified/server/Application.java
@@ -1,5 +1,6 @@
 package com.bitunified.server;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -9,13 +10,18 @@ import javax.ws.rs.core.MediaType;
 public class Application {
 
         @POST
-        @Path("data")
+        @Path("/data")
         @Produces(MediaType.TEXT_PLAIN)
         public String input() {
                 return "ok";
         }
 
 
-
+        @GET
+        @Path("/health")
+        @Produces(MediaType.TEXT_PLAIN)
+        public String healthCheck() {
+                return "ok";
+        }
 
 }
