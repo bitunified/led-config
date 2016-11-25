b1636d8ab4d4cec891c7a33dfdb95084ef878ed3
B. Verhelst <apc.verhelst@belastingdienst.nl>
2016-11-25 14:16:20 +0100

    Parser

--- configuration/src/main/java/com/bitunified/ledconfig/configuration/parser/steps/ParserTest.java
+++ configuration/src/main/java/com/bitunified/ledconfig/configuration/parser/steps/ParserTest.java
@@ -1,23 +1,20 @@
 package com.bitunified.ledconfig.configuration.parser.steps;
 
 import com.bitunified.ledconfig.domain.Model;
-import com.bitunified.ledconfig.domain.product.profile.Profile;
 import org.junit.Test;
 
-import java.util.List;
-
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 
 public class ParserTest {
 
     @Test
-    public void testParser(){
+    public void testParser() {
         Parser.init();
-        ParsedResult parsedResult= Parser.parse("B1M200");
+        ParsedResult parsedResult = Parser.parse("B1M200");
 
-        Model[] partResult=parsedResult.getParts().toArray(new Model[]{});
+        Model[] partResult = parsedResult.getParts().toArray(new Model[]{});
         System.out.println(partResult[0]);
-        assertEquals("","[[[name:liniLED Aeris Profiel L20 props:{}] MaxDim: null], [[name:liniLED RGB Deco props:{section=key:section value:}] MaxDim: [Width:200 Height:null Depth:null Unit:null]]]",parsedResult.getParts().toString());
+        assertEquals("", "[[[name:liniLED Aeris Profiel L20 props:{}] MaxDim: null], [[name:liniLED RGB Deco props:{section=key:section value:}] MaxDim: [Width:200 Height:null Depth:null Unit:null]]]", parsedResult.getParts().toString());
     }
 }
