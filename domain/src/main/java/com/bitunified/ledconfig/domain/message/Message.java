ff672eb4ecc9b5e03933593f113a6fc6e9479416
BertLenovo <info@bitunified.nl>
2016-12-08 16:10:45 +0100

    Error messages

--- domain/src/main/java/com/bitunified/ledconfig/domain/message/Message.java
+++ domain/src/main/java/com/bitunified/ledconfig/domain/message/Message.java
@@ -6,7 +6,7 @@ public class Message {
 
     private String message;
 
-    private MessageStatus status=MessageStatus.ERROR;
+    private MessageStatus status=MessageStatus.UNKNOWN;
 
     public Message() {
 
--- domain/src/main/java/com/bitunified/ledconfig/domain/message/MessageStatus.java
+++ domain/src/main/java/com/bitunified/ledconfig/domain/message/MessageStatus.java
@@ -2,5 +2,10 @@ package com.bitunified.ledconfig.domain.message;
 
 
 public enum MessageStatus {
-    ERROR,WARNING
+    ERROR("red"), UNKNOWN("black"), WARNING("orange");
+    private final String color;
+
+     MessageStatus(String color){
+        this.color=color;
+    }
 }
